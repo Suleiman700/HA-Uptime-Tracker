cbe6dfe5d815e68069b77aec447d439cdeac46c4
Suleiman <soleman630@gmail.com>
2024-07-10 23:33:52 +0300

    Update sensor.py

--- custom_component/uptime_tracker/sensor.py
+++ custom_component/uptime_tracker/sensor.py
@@ -4,7 +4,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
-from datetime import datetime, timedelta
+from datetime import datetime
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,7 +20,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     sensors = [
         LastOfflineDurationSensor(hass, name),
-        LastOfflineSensor(hass, name),
+        LastOnlineSensor(hass, name),
     ]
 
     async_add_entities(sensors, update_before_add=True)
@@ -54,11 +54,11 @@ class LastOfflineDurationSensor(UptimeTrackerSensor):
 
     def _update_state(self):
         if not self._initialized:
-            last_offline_state = self._hass.states.get('sensor.uptime_tracker_last_offline')
-            if last_offline_state and last_offline_state.state:
-                last_offline_time = datetime.fromisoformat(last_offline_state.state)
+            last_online_state = self._hass.states.get('sensor.uptime_tracker_last_online')
+            if last_online_state and last_online_state.state:
+                last_online_time = datetime.fromisoformat(last_online_state.state)
                 current_time = datetime.now()
-                duration_seconds = (current_time - last_offline_time).total_seconds()
+                duration_seconds = (current_time - last_online_time).total_seconds()
                 self._state = round(duration_seconds)
                 self._hass.states.async_set('sensor.uptime_tracker_last_offline_duration', self._state, {"friendly_name": "Last Offline Duration"})
                 self._initialized = True
@@ -66,11 +66,11 @@ class LastOfflineDurationSensor(UptimeTrackerSensor):
                 self._state = None
             _LOGGER.debug(f"{self._name} state updated to: {self._state}")
 
-class LastOfflineSensor(UptimeTrackerSensor):
+class LastOnlineSensor(UptimeTrackerSensor):
     def __init__(self, hass, name):
-        super().__init__(hass, name, 'Last Offline')
+        super().__init__(hass, name, 'Last Online')
 
     def _update_state(self):
         self._state = datetime.now().isoformat()
-        self._hass.states.async_set('sensor.uptime_tracker_last_offline', self._state, {"friendly_name": "Last Offline"})
+        self._hass.states.async_set('sensor.uptime_tracker_last_online', self._state, {"friendly_name": "Last Online"})
         _LOGGER.debug(f"{self._name} state updated to: {self._state}")
